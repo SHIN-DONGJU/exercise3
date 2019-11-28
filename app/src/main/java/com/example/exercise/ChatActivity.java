160162c06fc4f0d2e5f18ab26173206d13d7027f
SHIN-DONGJU <tlsehdwn95@naver.com>
2019-11-29 02:03:51 +0900

    stt 버전

--- app/src/main/java/com/example/exercise/ChatActivity.java
+++ app/src/main/java/com/example/exercise/ChatActivity.java
@@ -18,6 +18,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import android.widget.Toast;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.RecognitionListener;
+import android.content.Intent;
+import androidx.core.app.ActivityCompat;
+import android.os.Build;
+import android.Manifest;    // permission  관련 import
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,21 +39,44 @@ public class ChatActivity extends AppCompatActivity {
     private String nick;
     public String type;
 
+    Intent intent;
+    SpeechRecognizer mRecognizer;   // stt 관련 변수 선언
+
     private EditText EditText_chat;
-    private Button Button_send;
+    private Button Button_send, Button_send2;
     private DatabaseReference myRef;
+
+    final int PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        Intent intent =getIntent();
+        if (Build.VERSION.SDK_INT >= 23) {
+            // 퍼미션 체크
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.INTERNET,
+                    Manifest.permission.RECORD_AUDIO}, PERMISSION);
+        }
+
+        EditText_chat = (EditText) findViewById(R.id.EditText_chat);
+        Button_send = (Button) findViewById(R.id.Button_send);
+        Button_send2 = (Button) findViewById(R.id.Button_send2);
+
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
+
+        Button_send2.setOnClickListener(v -> {
+            mRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+            mRecognizer.setRecognitionListener(listener);
+            mRecognizer.startListening(intent);
+        });
+
+        Intent intent = getIntent();
         nick=intent.getExtras().getString("nickname");
         type=intent.getExtras().getString("type");
-
-        Button_send = findViewById(R.id.Button_send);
-        EditText_chat = findViewById(R.id.EditText_chat);
 
         Button_send.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,4 +140,80 @@ public class ChatActivity extends AppCompatActivity {
         //2. db 내용을 넣는다.
         //3. 상대방 폰에 채팅 내용이 보임.
     }
+
+            RecognitionListener listener = new RecognitionListener() {
+                @Override
+                public void onReadyForSpeech(Bundle params) {
+                    Toast.makeText(getApplicationContext(),"음성인식을 시작합니다.",Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onBeginningOfSpeech() {}
+
+                @Override
+                public void onRmsChanged(float rmsdB) {}
+
+                @Override
+                public void onBufferReceived(byte[] buffer) {}
+
+                @Override
+                public void onEndOfSpeech() {}
+
+                @Override
+                public void onError(int error) {
+                    String message;
+
+                    switch (error) {
+                        case SpeechRecognizer.ERROR_AUDIO:
+                            message = "오디오 에러";
+                            break;
+                        case SpeechRecognizer.ERROR_CLIENT:
+                            message = "클라이언트 에러";
+                            break;
+                        case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                            message = "퍼미션 없음";
+                            break;
+                        case SpeechRecognizer.ERROR_NETWORK:
+                            message = "네트워크 에러";
+                            break;
+                        case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                            message = "네트웍 타임아웃";
+                            break;
+                        case SpeechRecognizer.ERROR_NO_MATCH:
+                            message = "찾을 수 없음";
+                            break;
+                        case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                            message = "RECOGNIZER가 바쁨";
+                            break;
+                        case SpeechRecognizer.ERROR_SERVER:
+                            message = "서버가 이상함";
+                            break;
+                        case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                            message = "말하는 시간초과";
+                            break;
+                        default:
+                            message = "알 수 없는 오류임";
+                            break;
+                    }
+
+                    Toast.makeText(getApplicationContext(), "에러가 발생하였습니다. : " + message,Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onResults(Bundle results) {
+                    // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줍니다.
+                    ArrayList<String> matches =
+                            results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                    for(int i = 0; i < matches.size() ; i++){
+                        EditText_chat.setText(matches.get(i));
+                    }
+                }
+
+                @Override
+                public void onPartialResults(Bundle partialResults) {}
+
+                @Override
+                public void onEvent(int eventType, Bundle params) {}
+            };
 }
